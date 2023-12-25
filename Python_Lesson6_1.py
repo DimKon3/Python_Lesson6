aa93cd29d1398d04e9d654dec679b224c5bba25d
DimKon3 <142818063+DimKon3@users.noreply.github.com>
2023-12-25 16:05:07 +0300

    Add files via upload

--- Python_Lesson6_1.py
+++ Python_Lesson6_1.py
@@ -1,7 +1,7 @@
 count = int(input("Сколько чисел будем вводить: "))
 kol_zerro = 0
 for i in range(count):
-    num = int(input("Введите очередное число:"))
+    num = int(input(f"Введите {i+1} число: "))
     if num == 0:
         kol_zerro += 1
 
